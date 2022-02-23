30f3b26f4229a943eb453ee1359d2325ea15ee41
pscloud20 <phillipossc20@gmail.com>
2022-02-23 16:29:28 +0000

    task1 success

--- simfuncs.py
+++ simfuncs.py
@@ -13,6 +13,7 @@ import tqdm
 import random
 from matplotlib.colors import LogNorm
 import matplotlib
+import time as t
 
 
 
@@ -27,105 +28,9 @@ def randomprob(probability):
         return 1
     if y >= probability:
         return 2
-
+    
+    
 #%%
-
-#defining site 1 in mathematical oslo model as site 0
-
-class oslo:
-    
-    def __init__(self, L, prob): #calling class objects length of model and probability
-        self.L = L 
-        self.p = prob
-        self.z = np.zeros(L) #initialise array length L that contains slopes at each site (initially 0 will add values later)
-        self.h = np.zeros(L) #same for heights as slopes use np.zeros over [] as it allows easier manipulation of elements and axes later on
-        self.thresholdz = []
-        self.sites = []
-        self.s_size = []
-        
-    def s_size(self):
-        return s_size
-        
-    def drive(self): #define a drive function for adding grain to site_0 
-        self.z[0] = self.z[0] +1 #definition of drive is adding 1 grain to the first site in the model
-        self.h[0] = self.z[0] +1 #height of site_0 increases by 1
-        
-    def relax(self):
-        sites_relaxed = []
-        sites_totalrelax = False 
-        self.s = 0 #avalanche size
-        
-# =============================================================================
-#         while len(sites_relaxed) > 0:
-#             sites_relaxed = []
-#             
-#             for i in range(self.L):
-#             if self.z[i] > self.thresholdz[i]:
-#                 #sites_relaxed.append(i)
-#                     
-#             for i in sites_relaxed:
-#                 self.s = self.s +1
-#                 self.h[i] = self.h[i]+1
-# =============================================================================
-        
-#When system is not relaxed ie when slope[i] is larger than threshold, the total system is False
-        for i in range(self.L):
-            if self.z[i] > self.thresholdz[i]:
-                #sites_relaxed.append(i)
-                sites_totalrelax = False
-                break #ensures inner loop stops executing
-            else:
-                sites_totalrelax = True
-                
-        while sites_totalrelax is False: 
-            if self.z[i] > self.thresholdz[i]:
-            
-# =============================================================================
-#                 if i != self.L -1:
-#                     self.h[i+1] = self.h[i+1] +1
-# =============================================================================
-                    
-                if i == 0: #behaviour of site 0 when relaxing
-                    self.z[i] = self.z[i] -2
-                    self.z[i+1] = self.z[i+1] + 1 #site 1 has a grain added since site 0 relaxes
-                
-                elif i !=0 or i != self.__L-1: #i.e. for i =2, ...
-                    self.z[i] = self.z[i]-2
-                    self.z[i+1] = self.z[i+1] +1
-                    self.z[i-1] = self.z[i-1] +1
-            
-                elif i == self.L-1:
-                    self.z[i] = self.z[i]-1 #slope of L site -1 as it falls off grid
-                    self.z[i-1] = self.z[i-1] +1 #slope of L-1 site +1 as L site loses grain
-                
-                self.thresholdz[i] = randomprob(self.p)
-                
-                
-    def animate(self):
-        grid = grid(self)
-        for i in range(self.L):
-            for j in range(self.heights[i]):
-                grid.addgrain(i+1)
-    
-    def h(self):
-        return self.h
-    def h_1(self):
-        return self.h_data
-    def steadystate(self):
-        for i in range(len(self.h_data)):
-            if self.h_data[i] > np.max(self.h_1[len(self.h_data())-100:])
-    def mean_h(self):
-        return np.mean(self.h_data[self.steadystate:])
-    
-    
-                
-            
-            
-                
-                
-                
-            
-#create a class that sets up animation for grid
 class grid:
     
     def __init__(self, L):
@@ -170,9 +75,115 @@ class grid:
         plt.grid()
         plt.show()
         
+
+#%%
+
+#defining site 1 in mathematical oslo model as site 0
+
+class oslo:
+    
+    def __init__(self, L, prob): #calling class objects length of model and probability
+        self.L = L 
+        self.p = prob
+        self.z = np.zeros(L) #initialise array length L that contains slopes at each site (initially 0 will add values later)
+        self.h = np.zeros(L) #same for heights as slopes use np.zeros over [] as it allows easier manipulation of elements and axes later on
+        self.h1_data = []
+        self.thresholdz = []
+        self.sites = []
+        self.s_size = []
         
+    def s_size(self):
+        return self.s_size
         
+    def drive(self): #define a drive function for adding grain to site_0 
+        self.z[0] = self.z[0] +1 #definition of drive is adding 1 grain to the first site in the model
+        self.h[0] = self.h[0] +1 #height of site_0 increases by 1
         
+    def relax(self): #follows structure of mathematical algorithm of oslo model relaxation
+        sites_relaxed = []
+        sites_totalrelax = False 
+        self.s = 0 #avalanche size
+        
+        while len(sites_relaxed) > 0:
+            sites_relaxed = []
+            
+            for i in range(self.L):
+                if self.z[i] > self.thresholdz[i]:
+                    sites_relaxed.append(i)
+                    
+            for i in sites_relaxed:
+                self.s = self.s +1
+                self.h[i] = self.h[i]+1
+        
+#When system is not relaxed ie when slope[i] is larger than threshold, the total system is False
+# =============================================================================
+#         for i in range(self.L):
+#             if self.z[i] > self.thresholdz[i]:
+#                 #sites_relaxed.append(i)
+#                 sites_totalrelax = False
+#                 break #ensures inner loop stops executing
+#             else:
+#                 sites_totalrelax = True
+#                 
+#         while sites_totalrelax is False: 
+#             if self.z[i] > self.thresholdz[i]:
+# =============================================================================
+            
+                if i != self.L -1:
+                    self.h[i+1] = self.h[i+1] +1
+                    
+                if i == 0: #behaviour of site 0 when relaxing
+                    self.z[i] = self.z[i] -2
+                    self.z[i+1] = self.z[i+1] + 1 #site 1 has a grain added since site 0 relaxes
+                    self.steadystate = False
+                elif i !=0 or i != self.__L-1: #i.e. for i =2, ...
+                    self.z[i] = self.z[i]-2
+                    self.z[i+1] = self.z[i+1] +1
+                    self.z[i-1] = self.z[i-1] +1
+                    self.steadystate = False
+                elif i == self.L-1:
+                    self.z[i] = self.z[i]-1 #slope of L site -1 as it falls off grid
+                    self.z[i-1] = self.z[i-1] +1 #slope of L-1 site +1 as L site loses grain
+                    self.steadystate = True
+                self.thresholdz[i] = randomprob(self.p)
+                
+    def sim(self, N, animate = False, av_limit = False, av_data = False,  h1_data = False):
+        for i in range(N):
+            self.drive()
+            self.relax()
+            if animate == True:
+                t.sleep(0)
+            if np.all(h1_data) == True:
+                self.h1_data.append(self.h[0])
+            if av_limit == True: #loop that checks if the limit of avalanche reached ie grain fallen off and steady state have been reached if yes then loop breaks
+                if self.steadystate == True:
+                    break
+            if av_data == True:
+                if self.steadystate == True:
+                    self.s_size = self.s
+            
+        
+                
+                
+    def animate(self):
+        grid = grid(self)
+        for i in range(self.L):
+            for j in range(self.heights[i]):
+                grid.addgrain(i+1)
+    
+    def h(self):
+        return self.h
+    def h1(self):
+        return self.h1_data
+    def steadystate(self):
+        for i in range(len(self.h1_data)):
+            if self.h1_data[i] > np.max(self.h1[len(self.h1_data())-100:]):
+                return i+100
+    
+    def mean_h(self):
+        return np.mean(self.h_data[self.steadystate:])
+    def N_grains(self):
+        return np.sum(self.h)
         
             
 #%%
--- testing.py
+++ testing.py
@@ -17,3 +17,22 @@ boundaries = [0,0,1,1]
 norm = matplotlib.colors.BoundaryNorm(boundaries, ncolors.N)
         
 plt.pcolormesh(anim_matrix, norm = norm, cmap = ncolors)
+
+#%%
+
+N=1000
+L=16
+
+test_1 = oslo(L, 0.5)
+test_1.sim(N, h1_data = True)
+h1 = test_1.h1()
+
+print(len(h1))
+
+
+plt.plot(np.arange(0,N, 1), h1)
+plt.ylim(0,35)
+plt.vlines(300,0,35)
+mean  = np.mean(h1)
+print(mean)
+
